4a41a79f6ad20cfd59aeece572ee59b9611551b1
Sreeram <sreeramkandakatla@gmail.com>
2025-05-11 17:03:28 +0530

    Update vite.config.js for production

--- bb_frontend/vite.config.js
+++ bb_frontend/vite.config.js
@@ -1,17 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [react(),tailwindcss(),],
+//   server: {
+//     port:5173,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',  // or whatever port your backend runs
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     }
+//   }
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  server: {
-    port:5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',  // or whatever port your backend runs
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+  plugins: [react(), tailwindcss()],
+});
